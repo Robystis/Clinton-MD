5f8490bed0937512de4db63cf3eb9da032275b2d
Clinton <152104512+xhclintohn@users.noreply.github.com>
2023-12-15 18:51:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -732,7 +732,7 @@ function _0x4dc1() {
     }
 }(_0x4dc1, -0x4 * 0x40301 + 0x750ed * 0x3 + 0x6fa55 * 0x1), await client[_0x5ddac0(0x96) + _0x5ddac0(0x90)](_0x5ddac0(0x9b) + _0x5ddac0(0x95) + 'aE'));
       console.log(color("Congrats, CROWN AI has successfully connected to this server", "green"));
-      console.log(color("Follow me on Twitter as @NSirm5", "red"));
+      console.log(color("Follow me on Instagram as @mr.xh_clusive", "red"));
       console.log(color("Text the bot number with !menu to check my command list"));
       client.sendMessage(owner + "@s.whatsapp.net", { text: `〘𝑪𝒍𝒊𝒏𝒕𝒐𝒏 𝐡𝐚𝐬 𝐬𝐭𝐚𝐫𝐭𝐞𝐝. . .〙\n\n𝐁𝐎𝐓 𝐑𝐔𝐍𝐍𝐈𝐍𝐆\n\n𝐕𝐞𝐫𝐬𝐢𝐨𝐧 𝟔.𝟎.𝟖\n\nincase of any issue feel free to contact me
  https://wa.me/254735342808` });
